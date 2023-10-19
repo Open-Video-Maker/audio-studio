a4ec6ab0f788be4d2b481fbd093fc70d368b0dfe
Zhi.Huang <zh.impact@outlook.com>
2023-10-19 17:06:29 +0800

    feat: server add swagger and cors

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,12 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
+import { swagger } from "@elysiajs/swagger";
 
 import { APIV1 } from "./api/v1";
 
 const app = new Elysia()
+  .use(cors())
+  .use(swagger())
   .group("/api/v1", (app) => app.use(APIV1))
   .get("/", () => "Audio Studio API")
   .listen(3000);
