1e187eeafb7ab711f8418753ccb63110df1b4552
Zhi.Huang <zh.impact@outlook.com>
2023-10-19 19:03:35 +0800

    update: server API index

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,20 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
+import { html } from "@elysiajs/html";
 import { swagger } from "@elysiajs/swagger";
 
 import { APIV1 } from "./api/v1";
 
 const app = new Elysia()
   .use(cors())
+  .use(html())
   .use(swagger())
   .group("/api/v1", (app) => app.use(APIV1))
-  .get("/", () => "Audio Studio API")
+  .get("/", ({ html }) =>
+    html(
+      `<p style="font-family: monospace">Audio Studio API. Please view at <a href='/swagger'>/swagger</a></p>`
+    )
+  )
   .listen(3000);
 
 console.log(
